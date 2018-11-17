8a18458f4644ee353ece2585e315d8cc932e0ee1
Hyeonkwon Cho <chkwon91@gmail.com>
2018-11-17 10:07:57 +0900

    navigation bottom line first commit

--- app/src/main/java/com/kwony/mdpreview/MainActivity.java
+++ app/src/main/java/com/kwony/mdpreview/MainActivity.java
@@ -1,13 +1,35 @@
 package com.kwony.mdpreview;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                case R.id.action_markdown:
+                    break;
+                case R.id.action_recent:
+                    break;
+                case R.id.action_theme:
+                    break;
+                }
+                return true;
+            }
+        });
     }
 }
