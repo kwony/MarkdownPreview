5856cb77fedf2d5968a1868fae94f6617bec799d
Hyeonkwon Cho <chkwon91@gmail.com>
2018-11-17 12:43:08 +0900

    Pages for bottom navigation bar
    These pages are connected via ViewPager

--- app/src/main/java/com/kwony/mdpreview/MainActivity.java
+++ app/src/main/java/com/kwony/mdpreview/MainActivity.java
@@ -2,13 +2,17 @@ package com.kwony.mdpreview;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.kwony.mdpreview.Tabs.Pager.MainPagerAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,20 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                 case R.id.action_markdown:
+                    viewPager.setCurrentItem(0);
                     break;
                 case R.id.action_recent:
+                    viewPager.setCurrentItem(1);
                     break;
                 case R.id.action_theme:
+                    viewPager.setCurrentItem(2);
                     break;
                 }
                 return true;
             }
         });
+
+        viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
     }
 }
---
+++ app/src/main/java/com/kwony/mdpreview/Tabs/ThemeTab.java
@@ -0,0 +1,20 @@
+package com.kwony.mdpreview.Tabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kwony.mdpreview.R;
+
+public class ThemeTab extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_theme, container,false);
+        return v;
+    }
+}
