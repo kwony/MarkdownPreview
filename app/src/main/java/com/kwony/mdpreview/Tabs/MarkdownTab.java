ebbd25a5eca218188096630206ede8fcfdc24a1e
Hyeonkwon Cho <chkwon91@gmail.com>
2018-11-18 12:05:50 +0900

    Sliding Tabs: Preview/Code tab inside Markdown fragment
    Markdown tab has preview/code view pagers to see and edit markdown file.
    These tabs support horizontal scrolling apart from navigation bar.
    
    SlidingTab Libraries are added.
    - SlidingTabLayout.java
    - SlidingTabStrip.java

--- app/src/main/java/com/kwony/mdpreview/Tabs/MarkdownTab.java
+++ app/src/main/java/com/kwony/mdpreview/Tabs/MarkdownTab.java
@@ -2,19 +2,39 @@ package com.kwony.mdpreview.Tabs;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kwony.mdpreview.R;
+import com.kwony.mdpreview.Tabs.Pager.MarkdownPagerAdapter;
+import com.kwony.mdpreview.Tabs.SlidingTabLib.SlidingTabLayout;
 
 public class MarkdownTab extends Fragment {
+    SlidingTabLayout tabs;
+    MarkdownPagerAdapter adapter;
+    ViewPager pager;
+    CharSequence Titles[] = {"Preview", "Code"};
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_markdown, container,false);
+
+        pager = v.findViewById(R.id.markdown_pager);
+
+        // TODO: Search difference between getFragmentManager() and getChildFragmentManager()
+        adapter = new MarkdownPagerAdapter(getChildFragmentManager(), Titles, 2);
+        pager.setAdapter(adapter);
+
+        tabs = v.findViewById(R.id.markdown_sliding_tab);
+        tabs.setDistributeEvenly(true);
+        tabs.setViewPager(pager);
+
         return v;
     }
 }
