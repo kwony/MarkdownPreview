ef1476e727249b2f647f63874885b2e6f3a97848
Hyeonkwon Cho <chkwon91@gmail.com>
2018-11-18 12:05:50 +0900

    Sliding Tabs: Preview/Code tab inside Markdown fragment
    Markdown tab has preview/code view pagers to see and edit markdown file.
    These tabs support horizontal scrolling apart from navigation bar.
    
    SlidingTab Libraries are added.
    - SlidingTabLayout.java
    - SlidingTabStrip.java

---
+++ app/src/main/java/com/kwony/mdpreview/Tabs/MarkdownTabs/CodeTab.java
@@ -0,0 +1,20 @@
+package com.kwony.mdpreview.Tabs.MarkdownTabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kwony.mdpreview.R;
+
+public class CodeTab extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_code, container,false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/kwony/mdpreview/Tabs/Pager/MarkdownPagerAdapter.java
@@ -0,0 +1,67 @@
+package com.kwony.mdpreview.Tabs.Pager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.SparseArray;
+import android.view.ViewGroup;
+
+import com.kwony.mdpreview.Tabs.MarkdownTabs.CodeTab;
+import com.kwony.mdpreview.Tabs.MarkdownTabs.PreviewTab;
+
+public class MarkdownPagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs = 2;
+    CharSequence mTitles[];
+
+    SparseArray<Fragment> registeredFragments = new SparseArray<>();
+
+    public MarkdownPagerAdapter(FragmentManager fm, CharSequence titles[],
+                                int numOfTabs) {
+        super(fm);
+
+        this.mTitles = titles;
+        this.mNumOfTabs = numOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment tab = null;
+        switch (position) {
+        case 0:
+            tab = new PreviewTab();
+            break;
+        case 1:
+            tab = new CodeTab();
+            break;
+        }
+
+        return tab;
+    }
+
+    @Override
+    public int getCount() {
+        return this.mNumOfTabs;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles[position];
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+        registeredFragments.put(position, fragment);
+        return fragment;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        registeredFragments.remove(position);
+        super.destroyItem(container, position, object);
+    }
+
+    public Fragment getRegisteredFragment(int position) {
+        return registeredFragments.get(position);
+    }
+}
