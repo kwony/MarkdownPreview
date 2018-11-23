ab0628be3a3fc42276e8deef94eb9ddb2dca7efc
Hyeonkwon Cho <chkwon91@gmail.com>
2018-11-23 19:45:44 +0900

    Custom actionbar: Custom actionbar frame

--- app/src/main/java/com/kwony/mdpreview/MainActivity.java
+++ app/src/main/java/com/kwony/mdpreview/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.kwony.mdpreview.Tabs.Pager.MainPagerAdapter;
 import com.kwony.mdpreview.Tabs.Pager.MainViewPager;
@@ -15,10 +17,16 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     MainViewPager viewPager;
 
+    private ImageButton ibShare;
+    private ImageButton ibOpen;
+    private ImageButton ibSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionbarSetup();
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -41,5 +49,38 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager = findViewById(R.id.viewpager);
         viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private void actionbarSetup() {
+        View v;
+
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.action_bar_custom);
+        v = getSupportActionBar().getCustomView();
+
+        ibShare = v.findViewById(R.id.ib_share);
+        ibOpen = v.findViewById(R.id.ib_open);
+        ibSave = v.findViewById(R.id.ib_save);
+
+        ibShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ibOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ibSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
