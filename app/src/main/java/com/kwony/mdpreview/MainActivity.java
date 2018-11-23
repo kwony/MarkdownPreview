e895b2ecb96dc8e6947ff05076245c9ea8d729be
Hyeonkwon Cho <chkwon91@gmail.com>
2018-11-23 20:41:41 +0900

    Permission: Request READ/WRITE storage permission
    Request READ/WRITE permissions when opening a file

--- app/src/main/java/com/kwony/mdpreview/MainActivity.java
+++ app/src/main/java/com/kwony/mdpreview/MainActivity.java
@@ -1,13 +1,19 @@
 package com.kwony.mdpreview;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.kwony.mdpreview.Tabs.Pager.MainPagerAdapter;
 import com.kwony.mdpreview.Tabs.Pager.MainViewPager;
@@ -20,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton ibShare;
     private ImageButton ibOpen;
     private ImageButton ibSave;
+
+    private final static int ASK_OPEN_PERMISSION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,14 +73,17 @@ public class MainActivity extends AppCompatActivity {
         ibShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
 
         ibOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] {
+                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        ASK_OPEN_PERMISSION);
             }
         });
 
@@ -82,5 +93,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch(requestCode) {
+        case ASK_OPEN_PERMISSION:
+            for (int grantResult : grantResults) {
+                if (grantResult == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(getApplicationContext(), R.string.toast_unable_contiue_without_perm,
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+
+            break;
+        }
     }
 }
