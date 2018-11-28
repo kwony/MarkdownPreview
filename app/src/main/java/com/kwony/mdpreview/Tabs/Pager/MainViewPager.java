8cc26a56aa935932bdf1d94bfa68745b58f8d443
Hyeonkwon Cho <chkwon91@gmail.com>
2018-11-28 20:09:22 +0900

    MainViewPager: Disable dragging between MainViewPagers

--- app/src/main/java/com/kwony/mdpreview/Tabs/Pager/MainViewPager.java
+++ app/src/main/java/com/kwony/mdpreview/Tabs/Pager/MainViewPager.java
@@ -3,7 +3,6 @@ package com.kwony.mdpreview.Tabs.Pager;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 
 public class MainViewPager extends ViewPager {
@@ -13,7 +12,15 @@ public class MainViewPager extends ViewPager {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        /* Scrolling into ViewPages in MainActivity is disabled */
+        /* Dragging between ViewPages in MainActivity is disabled */
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        /* Parent class ViewPager uses onTouchEvent function only to trap the status
+         * mIsBeingDragged and performs dragging. As this app does not support dragging
+         * between ViewPages, it is emptied and returns false as default */
         return false;
     }
 }
