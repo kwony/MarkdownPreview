3ac94f9a196e116271183fba9d8a804f0aa7a4da
Hyeonkwon Cho <chkwon91@gmail.com>
2019-01-23 22:56:36 +0900

    SelectFileAdapter: Add webview inside adapter xml

--- app/src/main/java/com/kwony/mdpreview/Adapters/SelectFileAdapter.java
+++ app/src/main/java/com/kwony/mdpreview/Adapters/SelectFileAdapter.java
@@ -1,17 +1,27 @@
 package com.kwony.mdpreview.Adapters;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.kwony.mdpreview.FileInfo;
 import com.kwony.mdpreview.R;
+import com.kwony.mdpreview.SelectActivity;
+import com.kwony.mdpreview.Utilities.FileManager;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.io.File;
 import java.util.List;
 
 public class SelectFileAdapter extends RecyclerView.Adapter<SelectFileAdapter.ViewHolder> {
@@ -27,12 +37,14 @@ public class SelectFileAdapter extends RecyclerView.Adapter<SelectFileAdapter.Vi
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvFileName;
         CardView cardView;
+        WebView wvPreview;
 
         public ViewHolder(View view) {
             super(view);
 
             tvFileName = (TextView) view.findViewById(R.id.tvFileName);
             cardView = (CardView) view.findViewById(R.id.cardView);
+            wvPreview = (WebView) view.findViewById(R.id.wvPreview);
         }
     }
 
@@ -55,6 +67,26 @@ public class SelectFileAdapter extends RecyclerView.Adapter<SelectFileAdapter.Vi
                 // TODO: Return to MainActivity with leaving sending file id.
             }
         });
+
+        StringBuffer fileValue = FileManager.readFileValue(
+                Environment.getExternalStorageDirectory()
+                        + File.separator + mContext.getString(R.string.app_name),
+                fileInfo.getFileName());
+
+        Log.d(SelectFileAdapter.class.getSimpleName(), "fileValue: " + fileValue);
+
+        if (fileValue != null) {
+            Parser parser = Parser.builder().build();
+            Node document = parser.parse(fileValue.toString());
+            HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+            Log.d(SelectFileAdapter.class.getSimpleName(), "I am here 1");
+
+            holder.wvPreview.loadData(renderer.render(document),
+                    "text/html; charset=utf-8", "UTF-8");
+
+            Log.d(SelectFileAdapter.class.getSimpleName(), "I am here 2");
+        }
     }
 
     @Override
